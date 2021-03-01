0e5cb85b1021b926a293f2fbd41e5ef0a789c20a
Justin Shaw <thoushawnotpass@gmail.com>
2021-02-28 17:38:01 -0800

    this is a bad commit message

--- assets/js/clapping.js
+++ assets/js/clapping.js
@@ -1,31 +1,57 @@
 // Credit: https://medium.com/hackernoon/how-i-implemented-the-medium-clap-from-scratch-4a16ac90ad3b
 (function () {
+    let timer;
+    let countFromAPI = 0;
     let accCounter = 0;
-    let totalCount = 127;
-    const hitCounterURL = 'https://hitcounter.pythonanywhere.com/count';
+    let totalCount = 0;
+    const hitCounterURL = 'https://hitcounter.pythonanywhere.com/';
     const minDeg = 1;
     const maxDeg = 72;
+    const animationDuration = 700;
     const particlesClasses = [
-        {
-            class: "pop-top"
-        },
-        {
-            class: "pop-top-left"
-        },
-        {
-            class: "pop-top-right"
-        },
-        {
-            class: "pop-bottom-right"
-        },
-        {
-            class: "pop-bottom-left"
-        },
+        { class: "pop-top" },
+        { class: "pop-top-left" },
+        { class: "pop-top-right" },
+        { class: "pop-bottom-right" },
+        { class: "pop-bottom-left" },
     ];
 
-    document.getElementById('totalCounter').innerText = totalCount;
+    document.getElementById('totalCounter').classList.add('d-none');
+    document.getElementById('clap').onmouseover = onClapMouseover;
+    document.getElementById('clap').onclick = onClapClick;
 
-    document.getElementById('clap').onmouseover = function () {
+    setTimeout(() => getClapCount('/nocount').then((count) => init(count)), 0);
+
+    function init(count) {
+        totalCount = parseInt(count);
+        countFromAPI = parseInt(count);
+        document.getElementById('totalCounter').innerText = count;
+        console.log('Initial count is ' + count);
+    }
+
+    function onClapClick() {
+        const clap = document.getElementById('clap');
+        const particles = document.getElementById('particles');
+        const particles2 = document.getElementById('particles-2');
+        const particles3 = document.getElementById('particles-3');
+        clap.classList.add('clicked');
+        upClickCounter();
+        runAnimationCycle(clap, 'scale');
+        if (!particles.classList.contains('animating')) {
+            animateParticles(particles, animationDuration);
+        } else if (!particles2.classList.contains('animating')) {
+            animateParticles(particles2, animationDuration);
+        } else if (!particles3.classList.contains('animating')) {
+            animateParticles(particles3, animationDuration);
+        }
+        upTotalClickCounter();
+        if (timer) {
+            clearInterval(timer);
+        }
+        timer = setTimeout(makeDelayedCallsToAPI, animationDuration + 1);
+    }
+
+    function onClapMouseover() {
         let sonarClap = document.getElementById('sonar-clap');
         sonarClap.classList.add('hover-active');
         setTimeout(() => {
@@ -33,38 +59,21 @@
         }, 2000);
     }
 
-    document.getElementById('clap').onclick = async function () {
-        const clap = document.getElementById('clap');
-        const particles = document.getElementById('particles');
-        const particles2 = document.getElementById('particles-2');
-        const particles3 = document.getElementById('particles-3');
-        clap.classList.add('clicked');
-        upClickCounter();
-        getClapCount().then((count) => console.log("Count is " + count));
-        runAnimationCycle(clap, 'scale');
-
-        if (!particles.classList.contains('animating')) {
-            animateParticles(particles, 700);
-        } else if (!particles2.classList.contains('animating')) {
-            animateParticles(particles2, 700);
-        } else if (!particles3.classList.contains('animating')) {
-            animateParticles(particles3, 700);
-        }
-    }
-
     function upClickCounter() {
-        const clickCounter = document.getElementById("clicker");
-        const totalClickCounter = document.getElementById('totalCounter');
-
         accCounter++;
+        const clickCounter = document.getElementById("clicker");
         clickCounter.children[0].innerText = '+' + accCounter;
-        totalClickCounter.innerText = totalCount + accCounter;
-
         if (clickCounter.classList.contains('first-active')) {
             runAnimationCycle(clickCounter, 'active');
         } else {
             runAnimationCycle(clickCounter, 'first-active');
         }
+    }
+
+    function upTotalClickCounter() {
+        const totalClickCounter = document.getElementById('totalCounter');
+        totalClickCounter.classList.remove('d-none');
+        totalClickCounter.innerText = totalCount + accCounter;
         runAnimationCycle(totalClickCounter, 'fader');
     }
 
@@ -109,11 +118,24 @@
         particles.style.transform = `rotate(${randomRotationAngle})`;
     }
 
-    async function getClapCount() {
-        const xmlHttp = new XMLHttpRequest();
-        xmlHttp.withCredentials = true;
-        xmlHttp.open('GET', hitCounterURL, true);
-        xmlHttp.send(null);
-        return xmlHttp.responseText;
+    async function getClapCount(endpoint = "/count") {
+        try {
+            let url = hitCounterURL + endpoint;
+            const xmlHttp = new XMLHttpRequest();
+            xmlHttp.withCredentials = true;
+            xmlHttp.open('GET', url, false);
+            xmlHttp.send(null);
+            console.log(xmlHttp.responseText);
+            return xmlHttp.responseText;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    function makeDelayedCallsToAPI() {
+        if (totalCount + accCounter > countFromAPI) {
+            countFromAPI = parseInt(await getClapCount());
+            setTimeout(makeDelayedCallsToAPI, 0);
+        }
     }
 })();
